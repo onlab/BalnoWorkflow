5174d055124e4e1770d54109569c986fb7144f99
mfernandez <marcus.fernandez@rocket-internet.com.br>
2015-05-26 20:10:41 -0300

    Fixed event name

--- src/WorkflowEvents.php
+++ src/WorkflowEvents.php
@@ -7,7 +7,7 @@ interface WorkflowEvents
     const BEGIN_EXECUTION = 'begin_execution';
     const END_EXECUTION = 'end_execution';
 
-    const BEGIN_TRANSITION = 'start_transition';
+    const BEGIN_TRANSITION = 'begin_transition';
     const END_TRANSITION = 'end_transition';
 
     const STATE_CHANGED = 'state_changed';
